9af26d061a1deb91a6938a5f608aa5c98ba9e42c
YiyuanWei <yiyuanw1@student.unimelb.edu.au>
2022-06-16 21:01:45 +1000

    add responsive style to canvas

--- src/components/dogCanvas.js
+++ src/components/dogCanvas.js
@@ -1,34 +1,36 @@
 import React, { Component } from "react";
 import DogDisplay from "./dogDisplay";
+import "./dogCanvas.css";
 
-class DogCanvas extends Component{
+class DogCanvas extends Component {
+  constructor() {
+    super();
+    this.state = {
+      displays: [],
+    };
+  }
 
-    constructor(){
-        super();
-        this.state = {
-            displays: []
-        };
-    }
-    
-    fresh(){
-        window.location.reload(true)
-    }
+  fresh() {
+    window.location.reload(true);
+  }
 
-    render(){
-        return(
-            <div>
-                <DogDisplay />
-                <DogDisplay />
-                <DogDisplay />
-                <DogDisplay />
-                <DogDisplay />
-                <DogDisplay />
-                <DogDisplay />
-                <DogDisplay />
-                <button onClick={this.fresh}>Click to get new set of dogs</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="canvas">
+        <div className="displays">
+          <DogDisplay />
+          <DogDisplay />
+          <DogDisplay />
+          <DogDisplay />
+          <DogDisplay />
+          <DogDisplay />
+          <DogDisplay />
+          <DogDisplay />
+        </div>
+        <button onClick={this.fresh}>Click to get new set of dogs</button>
+      </div>
+    );
+  }
 }
 
 export default DogCanvas;
